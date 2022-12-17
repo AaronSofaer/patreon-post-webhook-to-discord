a4b5963e04a0281bd72e776951a28037e516c940
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-12-17 22:09:12 +1100

    feat: Curl based discord webhook test successful. Start of readme
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # patreon-post-webhook-to-discord
 Get a webhook from patreon and ship it to discord
+
+
+## Setting up discord Webhooks
+
+* Go to your server | Integrations
+* Choose "New Webhook"
+* Configure the webhook bot with a name and channel.
+* Copy webhook url (Save as a github secret BBS TODO)
+* Test the webhook 
---
+++ testDiscordWebhookPOST.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Need to use -L to follow the 302 https://stackoverflow.com/questions/18474690/is-there-a-way-to-follow-redirects-with-command-line-curl
+
+if [[ -f /tmp/testEpubAeroplanesAndDirigiblesOfWar.epub ]]; then
+	echo "Book exists"
+else
+curl -L "https://www.gutenberg.org/ebooks/793.epub3.images" > /tmp/testEpubAeroplanesAndDirigiblesOfWar.epub
+fi
+
+#curl -v -X POST "$1" -H "Content-Type:multipart/form-data" -F "payload_json={\"content\":\"Have a nice day\"};type=application/json"
+     
+
+
+curl -X POST "$1" \
+	 -H "Content-Type:multipart/form-data" \
+	 -F 'payload_json={"content":"Here is a normal message", "attachments":[{"id":0, "description": "Aeroplanes and Dirigibles of War by Frederick Arthur Ambrose Talbot", "filename": "AeroplanesAndDirigiblesOfWar.epub"}]};type=application/json' \
+	 -F "files[0]=@/tmp/testEpubAeroplanesAndDirigiblesOfWar.epub;filename='AeroplanesAndDirigiblesOfWar.epub';type=application/epub+zip;"
+#rm /tmp/testEpubAeroplanesAndDirigiblesOfWar.epub
